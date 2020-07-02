b37ffe7ec02ff11085f0d9f83485e762ef3fa68c
Matthias Doering <matthias.m.doering@deutschebahn.com>
2020-07-02 12:48:46 +0200

    introduce enum for special items

--- module/gilded_rose.py
+++ module/gilded_rose.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+from enum import Enum
+
+class SpecialItem(Enum):
+    AGED_BRIE = "Aged Brie"
+    BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
 
 class GildedRose(object):
 
@@ -7,27 +13,27 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name != SpecialItem.AGED_BRIE.value and item.name != SpecialItem.BACKSTAGE_PASS.value:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != SpecialItem.SULFURAS.value:
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == SpecialItem.BACKSTAGE_PASS.value:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != SpecialItem.SULFURAS.value:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != SpecialItem.AGED_BRIE.value :
+                    if item.name != SpecialItem.BACKSTAGE_PASS.value:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != SpecialItem.SULFURAS.value:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
