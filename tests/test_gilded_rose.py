68130d2008c1b4159d502e229ce8bac59ef84e28
Matthias Doering <matthias.m.doering@deutschebahn.com>
2020-07-02 11:17:04 +0200

    tests

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -90,5 +90,8 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(-1, items[0].sell_in)
 
+    def test_printout(self):
+        print(Item("foo", 0, 0))
+
 if __name__ == '__main__':
     unittest.main()
