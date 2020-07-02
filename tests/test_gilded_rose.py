30bf76810cdd0344f8a8c0fb0e893223841f95b1
Matthias Doering <matthias.m.doering@deutschebahn.com>
2020-07-02 11:17:04 +0200

    tests

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -35,8 +35,60 @@ class GildedRoseTest(unittest.TestCase):
 
     def test_selldate_passed_quality_degrades(self):
         """Quality degrades twice as fast once the sell_in date has passed."""
-        pass
+        items = [Item("foo", 0, 2)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
 
+    def test_quality_never_above_50(self):
+        """Quality does not increase beyond 50."""
+        items = [Item("Aged Brie", 5, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(50, items[0].quality)
+
+    def test_sulfuras_never_decreases_quality(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 5, 39)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(39, items[0].quality)
+
+    def test_sulfuras_never_has_to_be_sold(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 5, 39)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(5, items[0].sell_in)
+
+    def test_backstage_passes_qual_increases(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 39)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertGreater(items[0].quality, 39)
+
+    def test_backstage_quality_increases_by_two_at_10_days_or_less(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 4)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(6, items[0].quality)
+
+    def test_backstage_quality_increases_by_three_at_5_days_or_less(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 4)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(7, items[0].quality)
+
+    def test_backstage_quality_is_zero_after_concert(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 4)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+
+    def test_sell_in_can_be_negative(self):
+        """sell_in does not go below 0."""
+        items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-1, items[0].sell_in)
 
 if __name__ == '__main__':
     unittest.main()
